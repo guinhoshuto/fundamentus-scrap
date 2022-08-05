04e17b37ae7689d701fdf0ddbf9cecb3604b141d
Guilherme S <guinho.shuto@gmail.com>
2022-08-05 10:29:09 -0400

    adicao de created_at

--- main.py
+++ main.py
@@ -14,21 +14,9 @@ url_resultado = "https://www.fundamentus.com.br/resultado.php"
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 data = date.today().strftime('%Y-%m-%d')
+created_at = date.today().strftime('%Y-%m-%d %H:%M:%S')
 
 app = FastAPI()
-
-def scrap(url):
-    print(url)
-    nome = url.split('/')
-    nome = nome[-1].split('.')
-    nome = nome[0]
-    print(nome)
-    req = requests.get(url, headers=headers)
-    if req.status_code == 200:
-        print('deu')
-        df = pd.read_html(req.text)
-        # df[0].to_csv(data+'_'+nome+'.csv')
-        df[0].to_sql(nome, engine)
 
 @app.get("/")
 def scrap(url):
@@ -41,7 +29,8 @@ def scrap(url):
     if req.status_code == 200:
         print('deu')
         df = pd.read_html(req.text)
-        # df[0].to_csv(data+'_'+nome+'.csv')
+        df[0]['created_at'] = created_at
+        # df[0].to_csv(data+'_'+nome+'.csv') 
         df[0].to_sql(nome, engine)
         return {"message":"cadastrado com sucesso"}
 
